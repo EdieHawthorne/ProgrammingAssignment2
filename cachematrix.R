ea85c16fae0670823ba46c401b0e3fe8221652ab
Edie Hawthorne <edie.hawthorne@Edies-MBP.attlocal.net>
2018-06-17 09:43:31 -0400

    updated cachmatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Catching the mean of a Vector
+## for Coursera course
 
-## Write a short comment describing this function
+## This function creates a special object that stores a numerix matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setrow <- function() m <<- solve
+    getrow <- function() m
+    list(set = set, get = get,
+         setrow = setrow,
+         getrow = getrow)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getrow()    ## Return a matrix that is the inverse of 'x'
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setrow(m)
+    m
 }
